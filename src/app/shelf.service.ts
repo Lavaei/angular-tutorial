511e992bdfcad3d4c63a2f36e795dbbe1caa13cb
Mostafa Lavaei <twco@ymail.com>
2020-09-18 20:01:47 +0430

    Season2 - Chapter 8

--- src/app/shelf.service.ts
+++ src/app/shelf.service.ts
@@ -1,9 +1,17 @@
+import {HttpClient} from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {IProduct} from "./interfaces/IProduct";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShelfService {
 
-  constructor() { }
+  constructor(protected _http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]>
+  {
+    return this._http.get<IProduct[]>('assets/resources/products.json');
+  }
 }
--- src/app/shelf/shelf.component.ts
+++ src/app/shelf/shelf.component.ts
@@ -11,31 +11,13 @@ import {ShelfService} from "../shelf.service";
 export class ShelfComponent implements OnInit
 {
 
-  products: IProduct[] = [
-    {
-      _id:      '1',
-      image:    '1.jpg',
-      remained: 1
-    },
-    {
-      _id:      '2',
-      image:    '2.jpg',
-      remained: 0
-    },
-    {
-      _id:      '3',
-      image:    '3.jpg',
-      remained: 3
-    },
-    {
-      _id:      '4',
-      image:    '4.jpg',
-      remained: 5
-    },
-  ];
+  products: IProduct[] = [];
 
-  constructor()
+  constructor(protected _shelfService:ShelfService)
   {
+    this._shelfService.getProducts().subscribe(
+      (products) => this.products = products
+    );
   }
 
   ngOnInit(): void
