2f315262c0e0aab55bf1e6594c24fd57c044bd10
Mostafa Lavaei <twco@ymail.com>
2020-09-18 17:23:58 +0430

    Season2 - Chapter 4

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IProduct} from "../interfaces/IProduct";
 
 @Component({
   selector: 'app-product',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+
+  @Input('product') product: IProduct;
 
   constructor() { }
 
