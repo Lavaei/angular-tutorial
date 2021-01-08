8f9161d7abb4e93532ed6f9b533d347f73df9aff
Mostafa Lavaei <twco@ymail.com>
2021-01-08 19:09:09 +0330

    Season2 - Chapter 16

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+
+
 
   constructor() { }
 
