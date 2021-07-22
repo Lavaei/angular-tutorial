d01aec62f3c550a8abd3048845f6dfa2dfef30f7
Mostafa Lavaei <twco@ymail.com>
2021-07-22 17:54:17 +0430

    Season2 - Chapter 47

--- src/app/only-guests.guard.ts
+++ src/app/only-guests.guard.ts
@@ -1,19 +1,29 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AppService} from "./app.service";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
-export class OnlyGuestsGuard implements CanActivate {
+export class OnlyGuestsGuard implements CanActivate
+{
 
-	constructor(protected _appService:AppService)
+	constructor(protected _appService: AppService,
+	            protected _router: Router)
 	{
 	}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return !this._appService.user;
-  }
+	canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+	{
+		const PROFILE_URL: UrlTree = this._router.createUrlTree(['user', 'profile']);
+
+		if(this._appService.user)
+		{
+			return PROFILE_URL;
+		}
+
+		return true;
+	}
 
 }
