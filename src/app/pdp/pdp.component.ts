a5746f2ebd7b985b07ba285a1a40790f046bec70
Mostafa Lavaei <twco@ymail.com>
2021-10-29 21:03:42 +0330

    Season2 - Chapter 59

--- src/app/pdp/pdp.component.ts
+++ src/app/pdp/pdp.component.ts
@@ -14,6 +14,8 @@ import {ProductService} from "../product.service";
 export class PdpComponent implements OnInit
 {
 
+	rate: {[key:string] : number} = {};
+
 	options: {key: string, title: string}[] = [
 		{key: 'value', title: 'ارزش در برابر قیمت'},
 		{key: 'quality', title: 'کیفیت'}
@@ -97,5 +99,10 @@ export class PdpComponent implements OnInit
 	goToCart()
 	{
 		this._router.navigateByUrl("user/cart");
+	}
+
+	onComment()
+	{
+		console.log(this.rate);
 	}
 }
--- src/app/rate/rate.component.ts
+++ src/app/rate/rate.component.ts
@@ -1,16 +1,25 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ControlValueAccessor} from "@angular/forms";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-rate',
   templateUrl: './rate.component.html',
-  styleUrls: ['./rate.component.scss']
+  styleUrls: ['./rate.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			multi: true,
+			useExisting: RateComponent
+		}
+	]
 })
 export class RateComponent implements OnInit, ControlValueAccessor {
 
 	@Input('options') options: {key: string, title: string}[];
 
 	value: {[key:string] : number} = {};
+
+	onChange = (value: {[key:string] : number}) => {};
 
   constructor() { }
 
@@ -21,16 +30,25 @@ export class RateComponent implements OnInit, ControlValueAccessor {
 		}
   }
 
-	registerOnChange(fn: any): void
+
+	writeValue(value: {[key:string] : number}): void
 	{
+		this.value = value;
+	}
+
+	registerOnChange(onChange: (value: {[key:string] : number}) => {}): void
+	{
+		this.onChange = onChange;
+	}
+
+	setDisabledState(isDisabled: boolean): void
+	{
+
 	}
 
 	registerOnTouched(fn: any): void
 	{
 	}
 
-	writeValue(obj: any): void
-	{
-	}
 
 }
