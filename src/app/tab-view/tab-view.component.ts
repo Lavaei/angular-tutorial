e9c88d31101700011871f25c75c440052ab8428f
Mostafa Lavaei <twco@ymail.com>
2021-12-24 20:21:03 +0330

    Season2 - Chapter 67

--- src/app/tab-view/tab-view.component.ts
+++ src/app/tab-view/tab-view.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-tab-view',
   templateUrl: './tab-view.component.html',
-  styleUrls: ['./tab-view.component.scss']
+  styleUrls: ['./tab-view.component.scss'],
+	encapsulation: ViewEncapsulation.ShadowDom
 })
 export class TabViewComponent implements OnInit {
 
