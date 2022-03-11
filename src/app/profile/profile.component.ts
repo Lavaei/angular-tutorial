96aa63a9c7708c593892e3688448243d84a88471
Mostafa Lavaei <twco@ymail.com>
2022-03-11 19:54:48 +0330

    Season2 - Chapter 72

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {interval} from 'rxjs';
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	fullName: string = 'A';
+
+  constructor(protected _changeDetectionRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+
+		interval(100).pipe(
+			tap(() => this.fullName = Math.random().toString()),
+		).subscribe();
   }
 
+	updateDom(): void
+	{
+		this._changeDetectionRef.markForCheck();
+	}
 }
